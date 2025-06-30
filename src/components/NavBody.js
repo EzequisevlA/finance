c43e03294f2f09417a7615b6e8f109398cc40a72
EzequisevlA <ezequielmfsa@hotmail.com>
2025-06-30 18:56:39 +0100

    atualizacao 30/06/2025:18:55

---
+++ src/components/NavBody.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './style_components/Navbody.css'
+function NavBody() {
+  return (
+    <div className='psect'>
+        <div className='card'>
+            <h1>
+            Domine sua finanças com facilidade
+            </h1>
+            <div className='span_text'>
+              <span >
+                Descubra o app que simplifica sua gestão financeira e faça seu dinheiro render mais.
+              </span>
+            </div>
+            
+            <br></br>
+            <button>
+                Começar gratuitamente
+            </button>
+        </div>
+        <div className='celular'>
+
+        </div>
+        
+    </div>
+    
+    
+  )
+}
+
+export default NavBody
---
+++ src/components/NavHeader.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style_components/Navheader.css'
+
+function header() {
+  
+}
+
+export default header
